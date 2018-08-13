7ead4260214a72fba5e5570071d924d7575689d2
suhov-as <suhov.a.s@yandex.ru>
2018-08-13 16:24:29 +0300

    js плагины

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -16,7 +16,13 @@ class AppAsset extends AssetBundle
         'css/common.css',
     ];
     public $js = [
-        'js/main.min.js'
+        'js/jquery.min.js',
+        'js/owl.carousel.min.js',
+        'js/wNumb.js',
+        'js/nouislider.min.js',
+        'js/jquery.mCustomScrollbar.concat.min.js',
+        'js/jquery.validate.min.js',
+        'js/main.min.js',
     ];
     public $depends = [
         'yii\web\YiiAsset',
