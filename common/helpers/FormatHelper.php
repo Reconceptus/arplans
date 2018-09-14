243138d6115fba15e6a4e0d06724daf9c0e04544
suhov-as <suhov.a.s@yandex.ru>
2018-09-14 16:40:03 +0300

    Услуги в 2 столбца

---
+++ common/helpers/FormatHelper.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: borod
+ * Date: 14.09.2018
+ * Time: 16:28
+ */
+
+namespace common\helpers;
+
+
+class FormatHelper
+{
+    /**
+     * Делит массив на 2 части
+     * @param $array
+     * @param $keys
+     * @return array
+     */
+    public static function divideArray($array, $keys = false)
+    {
+        $count = count($array);
+        $partSize = ceil($count / 2);
+        return array_chunk($array, $partSize, $keys);
+    }
+}
--- modules/shop/widgets/services/views/index.php
+++ modules/shop/widgets/services/views/index.php
@@ -7,10 +7,22 @@
  */
 
 /* @var $models \modules\shop\models\Service[] */
+$models = \common\helpers\FormatHelper::divideArray($models);
 ?>
 <div class="custom-row">
     <div class="custom-row-col col-50">
-        <? foreach ($models as $model): ?>
+        <? foreach ($models[0] as $model): ?>
+            <article>
+                <h4 class="title"><?= $model->name ?>, <?= $model->price ?> &#8381;</h4>
+                <div class="text">
+                    <?= $model->preview_text ?>
+                </div>
+                <div class="link"><a href="#">Читать полностью</a></div>
+            </article>
+        <? endforeach; ?>
+    </div>
+    <div class="custom-row-col col-50">
+        <? foreach ($models[1] as $model): ?>
             <article>
                 <h4 class="title"><?= $model->name ?>, <?= $model->price ?> &#8381;</h4>
                 <div class="text">
