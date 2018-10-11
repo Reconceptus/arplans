175450d1d3c9ba69ceb31b33dd02cd5b9b79a340
suhov-as <suhov.a.s@yandex.ru>
2018-10-11 13:32:24 +0300

    формы логина, реги, ресета

--- frontend/models/SignupForm.php
+++ frontend/models/SignupForm.php
@@ -1,15 +1,16 @@
 <?php
+
 namespace frontend\models;
 
-use yii\base\Model;
 use common\models\User;
+use Yii;
+use yii\base\Model;
 
 /**
  * Signup form
  */
 class SignupForm extends Model
 {
-    public $username;
     public $email;
     public $password;
 
@@ -20,16 +21,11 @@ class SignupForm extends Model
     public function rules()
     {
         return [
-            ['username', 'trim'],
-            ['username', 'required'],
-            ['username', 'unique', 'targetClass' => '\common\models\User', 'message' => 'This username has already been taken.'],
-            ['username', 'string', 'min' => 2, 'max' => 255],
-
             ['email', 'trim'],
             ['email', 'required'],
             ['email', 'email'],
             ['email', 'string', 'max' => 255],
-            ['email', 'unique', 'targetClass' => '\common\models\User', 'message' => 'This email address has already been taken.'],
+            ['email', 'unique', 'targetClass' => '\common\models\User', 'message' => 'Пользователь с этим email уже зарегистрирован.'],
 
             ['password', 'required'],
             ['password', 'string', 'min' => 6],
@@ -46,13 +42,18 @@ class SignupForm extends Model
         if (!$this->validate()) {
             return null;
         }
-        
+
         $user = new User();
-        $user->username = $this->username;
+        $user->username = $this->email;
         $user->email = $this->email;
+        $user->status = User::STATUS_ACTIVE;
         $user->setPassword($this->password);
         $user->generateAuthKey();
-        
-        return $user->save() ? $user : null;
+        if($user->save()){
+            $userRole = Yii::$app->authManager->getRole('user');
+            Yii::$app->authManager->assign($userRole, $user->getId());
+            return $user;
+        }
+        return null;
     }
 }
--- frontend/views/site/resetPassword.php
+++ frontend/views/site/resetPassword.php
@@ -2,30 +2,41 @@
 
 /* @var $this yii\web\View */
 /* @var $form yii\bootstrap\ActiveForm */
+
 /* @var $model \frontend\models\ResetPasswordForm */
 
-use yii\helpers\Html;
 use yii\bootstrap\ActiveForm;
+use yii\helpers\Html;
 
-$this->title = 'Reset password';
+$this->title = 'Новый пароль';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="site-reset-password">
-    <h1><?= Html::encode($this->title) ?></h1>
 
-    <p>Please choose your new password:</p>
-
-    <div class="row">
-        <div class="col-lg-5">
-            <?php $form = ActiveForm::begin(['id' => 'reset-password-form']); ?>
-
-                <?= $form->field($model, 'password')->passwordInput(['autofocus' => true]) ?>
-
-                <div class="form-group">
-                    <?= Html::submitButton('Save', ['class' => 'btn btn-primary']) ?>
+<div class="section site-login">
+    <div class="content content--lg mobile-wide">
+        <div class="request--wrap gradient">
+            <div class="content content--xs">
+                <h1 class="title title-lg"><?= $this->title ?></h1>
+                <h2 class="subtitle align-center">Пожалуйста, введите новый пароль</h2>
+                <div class="login-form">
+                    <?php $form = ActiveForm::begin(['id' => 'reset-password-form']); ?>
+                    <div class="login-form--wrap">
+                        <div class="request-form--main custom-form">
+                            <div class="form-row-element">
+                                <div class="input">
+                                    <?= Html::activePasswordInput($model, 'password', ['autofocus' => true]) ?>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="form-row-submit">
+                            <div class="submit">
+                                <?= Html::submitButton('Изменить', ['class' => 'btn btn--lt', 'name' => 'login-button']) ?>
+                            </div>
+                        </div>
+                    </div>
+                    <?php ActiveForm::end(); ?>
                 </div>
-
-            <?php ActiveForm::end(); ?>
+            </div>
         </div>
     </div>
 </div>
