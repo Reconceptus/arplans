7b1b09a0e8d8f59b10b08d264896da26a5a39c14
suhov-as <suhov.a.s@yandex.ru>
2018-10-11 10:56:42 +0300

    Форма входа и скрипты

--- frontend/views/site/login.php
+++ frontend/views/site/login.php
@@ -4,36 +4,55 @@
 /* @var $form yii\bootstrap\ActiveForm */
 /* @var $model \common\models\LoginForm */
 
-use yii\helpers\Html;
 use yii\bootstrap\ActiveForm;
+use yii\helpers\Html;
 
 $this->title = 'Login';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
-<div class="site-login">
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <p>Please fill out the following fields to login:</p>
-
-    <div class="row">
-        <div class="col-lg-5">
-            <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
-
-                <?= $form->field($model, 'username')->textInput(['autofocus' => true]) ?>
-
-                <?= $form->field($model, 'password')->passwordInput() ?>
-
-                <?= $form->field($model, 'rememberMe')->checkbox() ?>
-
-                <div style="color:#999;margin:1em 0">
-                    If you forgot your password you can <?= Html::a('reset it', ['site/request-password-reset']) ?>.
+<div class="section site-login">
+    <div class="content content--lg mobile-wide">
+        <div class="request--wrap gradient">
+            <div class="content content--xs">
+                <h1 class="title title-lg">Вход</h1>
+                <h2 class="subtitle align-center">Пожалуйста, заполните форму</h2>
+                <div class="login-form">
+                    <?php $form = ActiveForm::begin(['id' => 'login-form']); ?>
+                        <div class="login-form--wrap">
+                            <div class="request-form--main custom-form">
+                                <div class="form-row-element">
+                                    <div class="input">
+                                        <?=Html::activeTextInput($model,'username',['placeholder'=>'*Email'])?>
+                                    </div>
+                                </div>
+                                <div class="form-row-element">
+                                    <div class="input">
+                                        <?=Html::activePasswordInput($model, 'password',['placeholder'=>'*Пароль'])?>
+                                    </div>
+                                </div>
+                                <div class="form-row-element">
+                                    <div class="check">
+                                        <label>
+                                            <?=Html::activeCheckbox($model, 'rememberMe',['label'=>false])?>
+                                            <span>Запомнить меня</span>
+                                        </label>
+                                    </div>
+                                </div>
+                                <div class="form-row-element">
+                                    <div class="form-text">
+                                        <p>Если вы забыли пароль, то можете <?= Html::a('сбросить его', ['site/request-password-reset']) ?>.</p>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="form-row-submit">
+                                <div class="submit">
+                                    <?= Html::submitButton('Войти', ['class' => 'btn btn--lt', 'name' => 'login-button']) ?>
+                                </div>
+                            </div>
+                        </div>
+                    <?php ActiveForm::end(); ?>
                 </div>
-
-                <div class="form-group">
-                    <?= Html::submitButton('Login', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
-                </div>
-
-            <?php ActiveForm::end(); ?>
+            </div>
         </div>
     </div>
 </div>
