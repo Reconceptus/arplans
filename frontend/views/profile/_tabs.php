75ba5655c1be9c304a9a02fab20c5e5348fd1899
suhov-as <suhov.a.s@yandex.ru>
2018-10-23 10:45:46 +0300

    профайл

---
+++ frontend/views/profile/_tabs.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: borod
+ * Date: 22.10.2018
+ * Time: 16:10
+ */
+?>
+<div class="tabs">
+    <div class="tab"><a href="/profile">Профайл</a></div>
+    <div class="tab"><a href="/profile/orders">Заказы</a></div>
+</div>
