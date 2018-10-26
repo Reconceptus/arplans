56b0b7e1e99564585a9c6070bb8a3f8941f5a5b6
suhov-as <suhov.a.s@yandex.ru>
2018-10-26 10:12:24 +0300

    продажи по апи в лк

--- frontend/views/profile/_tabs.php
+++ frontend/views/profile/_tabs.php
@@ -5,8 +5,14 @@
  * Date: 22.10.2018
  * Time: 16:10
  */
+$user = Yii::$app->user->identity;
+/* @var $user \common\models\User */
+$type = $user->access_token || $user->partner;
 ?>
 <div class="tabs">
     <div class="tab"><a href="/profile">Профайл</a></div>
     <div class="tab"><a href="/profile/orders">Заказы</a></div>
+    <? if ($type): ?>
+        <div class="tab"><a href="/profile/sales">Продажи</a></div>
+    <? endif; ?>
 </div>
