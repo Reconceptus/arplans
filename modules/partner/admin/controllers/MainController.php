5fb4fa147421bd09becc37958c0d80b24ca5841f
suhov-as <suhov.a.s@yandex.ru>
2018-11-01 16:59:45 +0300

    главная страница

--- modules/partner/admin/controllers/MainController.php
+++ modules/partner/admin/controllers/MainController.php
@@ -57,14 +57,12 @@ class MainController extends AdminController
             if ($video1 && $video1->tempName) {
                 $model->main_page_video_1 = $video1;
                 if ($model->validate(['main_page_video_1'])) {
-                    $dir = Yii::getAlias('@webroot/uploads/');
-                    FileHelper::createDirectory($dir . '/',777);
+                    $dir = Yii::getAlias('@webroot/uploads/videos/');
+                    FileHelper::createDirectory($dir . '/', 777);
                     $fileName = 'video1.' . $model->main_page_video_1->extension;
-                    if (file_exists($dir . '/' . $fileName) && is_file($dir . '/' . $fileName)) {
-                        unlink($dir . '/' . $fileName);
-                    }
-                    $model->main_page_video_1->saveAs($dir . '/' . $fileName);
-                    $model->main_page_video_1 = '/uploads/' . $fileName;
+                    $path = $dir . $fileName;
+                    $model->main_page_video_1->saveAs($path);
+                    $model->main_page_video_1 = '/uploads/videos/' . $fileName;
                 }
             }
             if (!$model->main_page_video_1 && isset($post['old_main_page_video_1'])) {
@@ -75,22 +73,22 @@ class MainController extends AdminController
             if ($video2 && $video2->tempName) {
                 $model->main_page_video_2 = $video2;
                 if ($model->validate(['main_page_video_2'])) {
-                    $dir = Yii::getAlias('@webroot/uploads/');
-                    FileHelper::createDirectory($dir . '/',777);
+                    $dir = Yii::getAlias('@webroot/uploads/videos/');
+                    FileHelper::createDirectory($dir . '/', 777);
                     $fileName = 'video2.' . $model->main_page_video_2->extension;
-                    if (file_exists($dir . '/' . $fileName) && is_file($dir . '/' . $fileName)) {
-                        unlink($dir . '/' . $fileName);
-                    }
-                    $model->main_page_video_2->saveAs($dir . '/' . $fileName);
-                    $model->main_page_video_2 = '/uploads/' . $fileName;
+                    $path = $dir . '/' . $fileName;
+                    $model->main_page_video_2->saveAs($path);
+                    $model->main_page_video_2 = '/uploads/videos/' . $fileName;
                 }
             }
             if (!$model->main_page_video_2 && isset($post['old_main_page_video_2'])) {
                 $model->main_page_video_2 = $post['old_main_page_video_2'];
             }
-            $model->save();
+            if ($model->save()) {
+                return $this->redirect('/admin/modules/partner/main');
+            }
         }
         $users = User::getAuthors();
-        return $this->render('index', ['model' => $model, 'users'=>$users]);
+        return $this->render('index', ['model' => $model, 'users' => $users]);
     }
 }
