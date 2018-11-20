b15ec63ba1b352f4e79b580d36bd41e4dced59a3
suhov-as <suhov.a.s@yandex.ru>
2018-11-20 13:19:41 +0300

    письма

--- common/mail/order-status-changed.php
+++ common/mail/order-status-changed.php
@@ -8,6 +8,158 @@
 
 /* @var $model \modules\shop\models\Order */
 ?>
+<?php
+/**
+ * Created by PhpStorm.
+ * User: adm
+ * Date: 19.11.2018
+ * Time: 10:56
+ */
 
-    <h3><?= $model->fio ?></h3>
-    Статус вашего заказа №<?= $model->id ?> изменен. Новый статус - <?= \modules\shop\models\Order::getStatusName($model->status) ?>
+/* @var $model \modules\shop\models\Order */
+?>
+    <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN""http://www.w3.org/TR/html4/loose.dtd">
+    <html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+        <!--[if !mso]><!-->
+        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+        <!--<![endif]-->
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Arplans</title>
+        <style type="text/css">
+            .ExternalClass {width:100%;}
+            .ExternalClass, .ExternalClass p, .ExternalClass span, .ExternalClass font, .ExternalClass td, .ExternalClass div {
+                line-height: 100%;
+            }
+            body {-webkit-text-size-adjust:none; -ms-text-size-adjust:none;}
+            body {margin:0; padding:0;}
+            table td {border-collapse:collapse;}
+            p {margin:0; padding:0; margin-bottom:0;}
+            h1, h2, h3, h4, h5, h6 {
+                color: #21202e;
+                line-height: 100%;
+            }
+            a, a:link {
+                color:#2196f3;
+                text-decoration: none;
+            }
+            body, #body_style {
+                background:#ffffff;
+                color:#21202e;
+                font-family: Arial, Helvetica, sans-serif;
+                font-size:12px;
+            }
+            span.yshortcuts { color:#21202e; background-color:none; border:none;}
+            span.yshortcuts:hover,
+            span.yshortcuts:active,
+            span.yshortcuts:focus {color:#21202e; background-color:none; border:none;}
+            a:visited { color: #2a7ec5; text-decoration: none}
+            a:focus   { color: #2a7ec5; text-decoration: underline}
+            a:hover   { color: #2a7ec5; text-decoration: underline}
+            @media only screen and (max-device-width: 480px) {
+                body[yahoo] #container1 {display:block !important}
+                body[yahoo] p {font-size: 10px}
+            }
+            @media only screen and (min-device-width: 768px) and (max-device-width: 1024px)  {
+                body[yahoo] #container1 {display:block !important}
+                body[yahoo] p {font-size: 12px}
+            }
+            /*@media only screen and (max-width: 640px), only screen and (max-device-width: 640px) {
+                div[class=hide-menu], td[class=hide-menu] {
+                    height: 0 !important;
+                    max-height: 0 !important;
+                    display: none !important;
+                    visibility: hidden !important;
+                }
+            }*/
+        </style>
+    </head>
+    <body style="color:#21202e; font-family: Arial, sans-serif; font-size:16px; background:#ffffff; " alink="#2196f3" link="#2196f3" bgcolor="#ffffff" text="#21202e" yahoo="fix">
+    <table width="100%" cellpadding="0" cellspacing="0" border="0">
+        <tr>
+            <td valign="top" align="center" width="600" style="width: 600px;">
+                <!--[if gte mso 10]>
+                <table align="center" width="600" border="0" cellspacing="0" cellpadding="0"><tr><td>
+                <![endif]-->
+
+                <table width="600" cellspacing="0" cellpadding="0" style="width: 100%; max-width: 600px; min-width: 320px;">
+                    <tr>
+                        <td bgcolor="#ffffff" style="border-top-style:solid; border-top-width: 3px; border-top-color: #2196f3; ">
+                            <table width="100%" cellspacing="0" cellpadding="0" border="0">
+                                <tbody>
+                                <tr>
+                                    <td style="padding-bottom: 20px; border-left-style:solid; border-left-width: 1px; border-left-color: #F0F1F3; border-right-style:solid; border-right-width: 1px; border-right-color: #F0F1F3; border-bottom-style:solid; border-bottom-width: 1px; border-bottom-color: #F0F1F3;">
+                                        <table width="100%" cellspacing="0" cellpadding="0" border="0">
+                                            <tr>
+                                                <td align="center" style="padding-top: 24px;">
+                                                    <table width="90%" cellspacing="0" cellpadding="0" border="0">
+                                                        <tr>
+                                                            <td align="center" style="height: 18px; font-size: 18px; line-height: 18px;">
+                                                                <a href="https://reconceptus.github.io/Arplans/dev/build/index.html" style="color: #21202e; text-decoration: none;" target="_blank">
+                                                                    <img width="170" height="45" title="ARPLANS" alt="ARPLANS" src="https://reconceptus.github.io/Arplans/dev/build/assets/images/logo.png" style="border:none; max-width: 170px; height: auto; max-height: 45px;">
+                                                                </a>
+                                                            </td>
+                                                        </tr>
+                                                    </table>
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td align="center" style="padding-top: 42px;">
+                                                    <table width="90%" border="0" cellspacing="0" cellpadding="0" style="text-align: left;">
+                                                        <tbody>
+                                                        <tr>
+                                                            <td style="padding-bottom: 16px; font-size: 17px; line-height: 22px;  font-family: Arial, sans-serif;  color:#000000; text-align: left;"><?= $model->fio ?></td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td style="color:#000000;  font-family: Arial, sans-serif; font-size:17px; line-height: 22px;">
+                                                                <p> Статус вашего заказа №<?= $model->id ?> изменен. Новый статус - <?= \modules\shop\models\Order::getStatusName($model->status) ?></p>
+                                                            </td>
+                                                        </tr>
+                                                        </tbody>
+                                                    </table>
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td align="center" style="padding-bottom: 5px; padding-top: 40px;">
+                                                    <table width="90%" border="0" cellspacing="0" cellpadding="0" style="text-align: left;">
+                                                        <tbody>
+                                                        <tr>
+                                                            <td align="center" style="font-size: 18px; line-height: 18px; padding: 10px 0; font-family: Arial, sans-serif; color:#30343f; text-align: center;">
+                                                                <a href="<?=Yii::$app->request->getHostInfo()?>" target="_blank" style="font-size: 18px; line-height: 18px; font-family: Arial, sans-serif; color:#2a7ec5; text-decoration: none;">arplans.ru</a>
+                                                            </td>
+                                                        </tr>
+                                                        </tbody>
+                                                    </table>
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td align="center" style="padding-top: 10px;">
+                                                    <table width="90%" border="0" cellspacing="0" cellpadding="0" style="text-align: center;">
+                                                        <tbody>
+                                                        <tr>
+                                                            <td style="font-size: 17px; line-height: 17px; padding: 10px 0; font-family: Arial, sans-serif; color:#30343f; text-align: center;"><?=\modules\content\models\ContentBlock::getValue('hot_line')?> &nbsp; <?=\modules\content\models\ContentBlock::getValue('phone')?> &nbsp; <?=\modules\content\models\ContentBlock::getValue('email')?></td>
+                                                        </tr>
+                                                        </tbody>
+                                                    </table>
+                                                </td>
+                                            </tr>
+                                        </table>
+                                    </td>
+                                </tr>
+                                </tbody>
+                            </table>
+                        </td>
+                    </tr>
+
+
+                </table>
+
+                <!--[if gte mso 10]>
+                </td></tr></table>
+                <![endif]-->
+            </td>
+        </tr>
+    </table>
+    </body>
+    </html>
