3bd2ff750d8a71f8243af5e3369d2b88e9e798f0
suhov-as <suhov.a.s@yandex.ru>
2018-11-29 13:27:39 +0300

    почта

--- common/config/params.php
+++ common/config/params.php
@@ -1,7 +1,7 @@
 <?php
 return [
     'adminEmail'                    => 'suhov.a.s@yandex.ru',
-    'supportEmail'                  => 'forsendtest@yandex.ru',
+    'supportEmail'                  => 'no-reply@arplans.ru',
     'user.passwordResetTokenExpire' => 3600,
     'defaultAvatar'                 => '/img/avatar.png',
     'defaultImage'                  => 'img/default.png'
