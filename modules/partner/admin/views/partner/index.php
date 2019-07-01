2f11237247a04bf75fa372291691bbcefe40f63e
suhov-as <suhov.a.s@yandex.ru>
2019-07-01 15:05:11 +0300

    админке

--- modules/partner/admin/views/partner/index.php
+++ modules/partner/admin/views/partner/index.php
@@ -46,7 +46,7 @@ $columns = [
         'template' => '{config} {categories} {delete}',
         'options'  => ['style' => 'width:100px'],
         'buttons'  => [
-            'delete' => function ($url, $model) {
+            'delete'     => function ($url, $model) {
                 return Html::a('<span class="glyphicon glyphicon-trash"></span>', Url::to([
                     '/partner/partner/delete',
                     'id'   => $model->id,
@@ -63,10 +63,13 @@ $columns = [
                     'back' => Yii::$app->request->absoluteUrl
                 ]));
             },
-            'config' => function ($url, $model) {
+            'config'     => function ($url, $model) {
+                if (!$model->agent_id) {
+                    return '';
+                }
                 return Html::a('<span class="glyphicon glyphicon-check"></span>', Url::to([
                     '/partner/partner/config',
-                    'id'   => $model->id
+                    'id' => $model->id
                 ]));
             }
         ]
@@ -74,8 +77,8 @@ $columns = [
 ];
 ?>
 <h1><?= $this->title ?></h1>
-<?= Html::a('Добавить партнера (быстро)', Url::to(['/partner/partner/add']), ['class' => 'btn btn-admin add-big-button']) ?>
-<?= Html::a('Добавить партнера', Url::to(['/partner/partner/create']), ['class' => 'btn btn-admin add-big-button']) ?>
+<?= Html::a('Добавить партнера в один клик', Url::to(['/partner/partner/add']), ['class' => 'btn btn-admin add-big-button', 'style'=>'margin-right:10px;']) ?>
+<?= Html::a('Добавить фирму-партнера', Url::to(['/partner/partner/create']), ['class' => 'btn btn-admin add-big-button']) ?>
 <?= \yii\grid\GridView::widget(
     [
         'dataProvider' => $dataProvider,
