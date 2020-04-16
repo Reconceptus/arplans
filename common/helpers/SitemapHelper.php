4f9c3995677884a5448289d1fcb85d527449f23e
venodon <venodon@gmail.com>
2020-04-16 13:44:35 +0300

    sitemap

---
+++ common/helpers/SitemapHelper.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace common\helpers;
+
+
+use modules\shop\models\Item;
+
+class SitemapHelper
+{
+    /**
+     * Generate sitemap
+     */
+    public static function sitemap()
+    {
+        $items = [];
+        $models = \Yii::$app->params['siteMapModels'];
+        if ($models) {
+            foreach ($models as $class)
+                $items = array_merge($items, $class::find()->active()->all());
+        }
+        $str = self::generate($items);
+        file_put_contents(\Yii::getAlias('@frontend') . '/web/sitemap.xml', $str);
+    }
+
+    /**
+     * @param $items Item[]
+     * @return string
+     */
+    public static function generate($items)
+    {
+        $host = \Yii::$app->params['front'];
+        $str = '<?xml version="1.0" encoding="UTF-8"?>' . PHP_EOL . '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">';
+        foreach ($items as $item) {
+            $str .= "<url>
+            <loc>" . $host . $item->getUrl() . "</loc>
+            <lastmod>" . date(DATE_W3C, strtotime($item->updated_at)) . "</lastmod>
+            <changefreq>weekly</changefreq>
+            </url>";
+        }
+        $str .= '</urlset>';
+        return $str;
+    }
+}
