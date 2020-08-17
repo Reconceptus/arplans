f8665d9cf34bf70cddd9f6623b5cb05c133b8887
venodon <venodon@gmail.com>
2020-08-17 09:41:12 +0300

    влиты правки в selection

--- modules/shop/frontend/views/compilation/selection.php
+++ modules/shop/frontend/views/compilation/selection.php
@@ -9,7 +9,35 @@ use yii\widgets\ListView;
 /* @var $favorites array */
 /* @var $selection Selection */
 
-$this->title = $selection->name;
+if ($_SERVER['REQUEST_URI']=='/shop/selection/proekty-dvuhetazhnyh-domov-i-kottedzhej') {
+    $this->title = 'Проекты двухэтажных домов и коттеджей купить онлайн';
+}elseif ($_SERVER['REQUEST_URI']=='/shop/selection/proekty-domov-i-kottedzhej-s-mansardoi') {
+    $this->title = 'Проекты домов и коттеджей с мансардой купить онлайн';
+}elseif ($_SERVER['REQUEST_URI']=='/shop/selection/proekty-odnoetazhnyh-domov-s-mansardoi') {
+    $this->title = 'Проекты одноэтажных домов и коттеджей с мансардой заказать онлайн';
+}elseif ($_SERVER['REQUEST_URI']=='/shop/selection/proekty-odnoetazhnyh-domov-s-terrasoj') {
+    $this->title = 'Проекты одноэтажных домов с террасой купить по доступной цене';
+}elseif ($_SERVER['REQUEST_URI']=='/shop/selection/proekty-odnoetazhnyh-domov-s-3-spalnyami') {
+    $this->title = 'Проекты одноэтажных домов с тремя спальнями купить в интернет-магазине';
+}elseif ($_SERVER['REQUEST_URI']=='/shop/selection/proekty-odnoetazhnyh-domov-do-100') {
+    $this->title = 'Проекты одноэтажных домов до 100 кв м купить с гарантией';
+}elseif ($_SERVER['REQUEST_URI']=='/shop/selection/proekty-odnoetazhnyh-domov-150') {
+    $this->title = 'Проекты домов одноэтажных до 150 м2 заказать онлайн';
+}elseif ($_SERVER['REQUEST_URI']=='/shop/selection/proekty-odnoetazhnyh-domov-iz-gazobetona') {
+    $this->title = 'Одноэтажные проекты домов из газобетона купить с гарантией';
+}elseif ($_SERVER['REQUEST_URI']=='/shop/selection/proekty-odnoetazhnyh-domov-iz-brusa') {
+    $this->title = 'Проекты одноэтажных домов из бруса купить готовый проект';
+}elseif ($_SERVER['REQUEST_URI']=='/shop/selection/proekty-odnoetazhnyh-domov-iz-kirpicha') {
+    $this->title = 'Проекты одноэтажных домов и коттеджей из кирпича купить по выгодной цене';
+}elseif ($_SERVER['REQUEST_URI']=='/shop/selection/proekty-domov-i-kottedzhej-s-garazhom') {
+    $this->title = 'Проекты домов и коттеджей с гаражом - купить готовый проект онлайн';
+}elseif ($_SERVER['REQUEST_URI']=='/shop/selection/proekty-domov-s-mansardoj-i-garazhom') {
+    $this->title = 'Проекты домов и коттеджей с мансардой и гаражом купить с гарантией';
+}elseif ($_SERVER['REQUEST_URI']=='/shop/selection/proekty-odnoetazhnyh-domov-i-kottedzhej') {
+    $this->title = 'Готовые проекты одноэтажных домов и коттеджей купить онлайн';
+}else {
+    $this->title = $selection->name;
+}
 $this->registerMetaTag(['name' => 'description', 'content' => $selection->description]);
 
 ?>
