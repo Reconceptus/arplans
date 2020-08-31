0f2050c9fd14d5bce0f7607f4b51da11062868c8
venodon <venodon@gmail.com>
2020-08-31 08:56:46 +0300

    telegram

--- common/helpers/TelegramTarget.php
+++ common/helpers/TelegramTarget.php
@@ -228,6 +228,7 @@ class TelegramTarget extends Target
      */
     private $messagesToRemove = [
         'Invalid auth key attempted for user',
-        'The verification code is incorrect'
+        'The verification code is incorrect',
+        'yii\web\HeadersAlreadySentException: Headers already sent.'
     ];
 }
